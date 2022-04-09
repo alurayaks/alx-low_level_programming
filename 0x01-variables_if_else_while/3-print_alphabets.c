95b8261eca45e3b0a45059f94b7138a104b9db92
alurayaks <alurayaks@gmail.com>
2022-04-09 01:00:54 +0100

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,17 +10,18 @@ int main(void)
 char lower_case;
 char upper_case;
 
-for (upper_case = 'A'; upper_case <= '2'; upper_case++)
-{
-putchar(upper_case);
-}
-
 for (lower_case = 'a'; lower_case <= '2'; lower_case++)
 {
 putchar(lower_case);
 }
 
-putchar('\n');
+for (upper_case = 'A'; upper_case <= '2'; upper_case++)
+{
+putchar(upper_case);
+}
+
+
+putchar ('\n');
 
 return (0);
 
