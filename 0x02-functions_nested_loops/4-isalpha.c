0d5098621843820a8c9bb6ea81b04cc613c97fc1
alurayaks <alurayaks@gmail.com>
2022-04-12 19:08:26 +0100

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+
+
+/**
+
+* _isalpha - Return 1 if c is a letter. lowercase or uppercase
+
+*
+
+*@c: The int to print
+
+* Return: Always 0.
+
+*/
+
+
+
+int _isalpha(int c)
+
+
+
+{
+
+
+
+if ((c > 'a' && c < 'z') || (C > 'A' && C < 'Z'))
+
+{
+
+return (1);
+
+}
+
+
+
+else
+
+{
+
+return (0);
+
+}
+
+
+
+}
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ *@c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ *@c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
+
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
